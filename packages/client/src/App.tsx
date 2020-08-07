470b53ab9b1be4ae36be39af11c4f27db8459bf2
Neo <alghoochi@gmail.com>
2020-08-07 20:01:36 +0430

    migrate to yarn workspaces + webpack configuration

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  background-color: pink;
+  padding: 0;
+`;
+
+const App: React.FC = () => (
+  <div>
+    <Div>
+      TEST
+    </Div>
+  </div>
+);
+
+export default App;
