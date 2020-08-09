bde5c6cb254c407497f6e56923482f37aa2f8cd4
Neo <alghoochi@gmail.com>
2020-08-09 23:27:08 +0430

    lightTheme

---
+++ packages/client/src/theme/light/index.ts
@@ -0,0 +1,14 @@
+import Colors from '~/types/theme/Colors';
+
+const colors: Colors = {
+  primary500: '#BB2299',
+  secondary100: '#F7F7FA',
+  gray100: '#FAFAFA',
+  gray200: '#BEBEBE',
+  gray300: '#AAAAAA',
+  gray400: '#757575',
+  gray500: '#606060',
+  gray600: '#404040',
+};
+
+export default colors;
---
+++ packages/client/src/theme/theme.ts
@@ -0,0 +1,52 @@
+import { DefaultTheme } from 'styled-components';
+import lightColors from './light';
+
+const theme: DefaultTheme = {
+  colors: lightColors,
+  transitions: {
+    fast: '150ms',
+    normal: '300ms',
+    slow: '500ms',
+    superSlow: '1000ms',
+  },
+  font: {
+    size: '16px',
+    family: 'roboto',
+  },
+  fontWeights: {
+    thin: 100,
+    light: 300,
+    regular: 400,
+    medium: 500,
+    bold: 700,
+    black: 900,
+  },
+  zIndex: {
+    zIndex0: 0,
+    zIndex1: 1,
+    zIndex10: 10,
+    zIndex100: 100,
+    zIndex1000: 1000,
+  },
+  breakpoints: {
+    sm: '576px',
+    md: '768px',
+    lg: '992px',
+    xl: '1200px',
+    xxl: '1600px',
+  },
+  borderRadiuses: {
+    sm: '5px',
+    md: '10px',
+    lg: '15px',
+    xl: '20px',
+    xxl: '30px',
+  },
+  sizes: {
+    layoutHorizontalPadding: '30px',
+    layoutVerticalPadding: '30px',
+    headerHeight: '104px',
+  },
+};
+
+export default theme;
