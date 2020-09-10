78533ead4c2775ca6302d93ce7073a40805fbada
Neo <alghoochi@gmail.com>
2020-09-10 20:00:24 +0430

    contentVerticalPadding, contentHorizontalPadding added to Sizes

--- packages/client/src/types/theme/Sizes.ts
+++ packages/client/src/types/theme/Sizes.ts
@@ -2,6 +2,8 @@ interface Sizes {
   layoutVerticalPadding: string,
   layoutHorizontalPadding: string,
   headerHeight: string,
+  contentVerticalPadding: string,
+  contentHorizontalPadding: string,
 }
 
 export default Sizes;
