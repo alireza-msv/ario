7d44ac46b4f54542fd18959273fbc4ef7bf9a963
Neo <alghoochi@gmail.com>
2020-09-10 19:52:31 +0430

    Container tests modifed

--- packages/client/test/theme/components/Container/index.test.tsx
+++ packages/client/test/theme/components/Container/index.test.tsx
@@ -1,30 +1,32 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
-import { theme } from '~/theme';
+import Theme, { theme } from '~/theme';
 import Container from '~/theme/components/Container';
+import { breakpointToMediaString } from '~/theme/utils/responsive';
+import Breakpoint from '~/types/theme/Breakpoint';
 import 'jest-styled-components';
 
 describe('Container test', () => {
   it('should have style rules', () => {
-    const { container } = render(<Container theme={theme} />);
+    const { container } = render(<Container />, { wrapper: Theme });
     expect(container.firstChild).toHaveStyleRule('width', '100%');
     expect(container.firstChild).toHaveStyleRule('padding-right', theme.sizes.layoutHorizontalPadding);
     expect(container.firstChild).toHaveStyleRule('margin-right', 'auto');
     expect(container.firstChild).toHaveStyleRule('margin-left', 'auto');
     expect(container.firstChild).toHaveStyleRule('width', '540px', {
-      media: `(min-width: ${theme.breakpoints.sm})`,
+      media: `(${breakpointToMediaString(Breakpoint.SM)})`,
     });
     expect(container.firstChild).toHaveStyleRule('width', '720px', {
-      media: `(min-width: ${theme.breakpoints.md})`,
+      media: `(${breakpointToMediaString(Breakpoint.MD)})`,
     });
     expect(container.firstChild).toHaveStyleRule('width', '960px', {
-      media: `(min-width: ${theme.breakpoints.lg})`,
+      media: `(${breakpointToMediaString(Breakpoint.LG)})`,
     });
     expect(container.firstChild).toHaveStyleRule('width', '1140px', {
-      media: `(min-width: ${theme.breakpoints.xl})`,
+      media: `(${breakpointToMediaString(Breakpoint.XL)})`,
     });
     expect(container.firstChild).toHaveStyleRule('width', '1500px', {
-      media: `(min-width: ${theme.breakpoints.xxl})`,
+      media: `(${breakpointToMediaString(Breakpoint.XXL)})`,
     });
   });
 });
