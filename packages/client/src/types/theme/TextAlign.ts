4613b24ce8b77a8bb28898368709bfdb9dc71b99
Neo <alghoochi@gmail.com>
2020-10-17 14:01:05 +0330

    theme TextAlign, TextDirection enums

---
+++ packages/client/src/types/theme/TextAlign.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+enum TextAlign {
+  Left = 'left',
+  Right = 'right',
+  Center = 'center',
+  Inherit = 'inherit',
+}
+
+export default TextAlign;
---
+++ packages/client/src/types/theme/TextDirection.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+enum TextDirection {
+  LTR = 'ltr',
+  RTL = 'rtl',
+  Inherit = 'inherit',
+}
+
+export default TextDirection;
