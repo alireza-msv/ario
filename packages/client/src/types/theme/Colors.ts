29aab098f10e907864b160383bf8fb34324f3d87
Neo <alghoochi@gmail.com>
2020-10-17 13:54:19 +0330

    theme primary and gray variants

--- packages/client/src/types/theme/Colors.ts
+++ packages/client/src/types/theme/Colors.ts
@@ -1,14 +1,27 @@
 interface Colors {
   background: string,
   foreground: string,
+  primary50: string,
+  primary100: string,
+  primary200: string,
+  primary300: string,
+  primary400: string,
   primary500: string,
+  primary600: string,
+  primary700: string,
+  primary800: string,
+  primary900: string,
   secondary100: string,
+  gray50: string,
   gray100: string,
   gray200: string,
   gray300: string,
   gray400: string,
   gray500: string,
   gray600: string,
+  gray700: string,
+  gray800: string,
+  gray900: string,
 }
 
 export default Colors;
