b3ee916f2b570d3ce2774e56d51110994b2925e3
Neo <alghoochi@gmail.com>
2020-10-23 20:50:03 +0330

    InputProps extends React.ComponentPropsWithoutRef<input>

--- packages/client/src/theme/components/Input/index.ts
+++ packages/client/src/theme/components/Input/index.ts
@@ -8,7 +8,7 @@ import {
   propsToBorderRadius,
 } from '~/theme/utils/input';
 
-export interface InputProps {
+export interface InputProps extends React.ComponentPropsWithoutRef<'input'> {
   direction?: TextDirection,
   textAlign?: TextAlign,
   sm?: boolean,
